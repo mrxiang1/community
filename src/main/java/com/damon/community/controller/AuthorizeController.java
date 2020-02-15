480aa898b12604ffbef4170f24c6aa44d37cd6e5
DamonXiang <545052197@qq.com>
2020-02-15 16:57:53 +0800

    add githubapp

---
+++ src/main/java/com/damon/community/controller/AuthorizeController.java
@@ -0,0 +1,31 @@
+package com.damon.community.controller;
+
+import com.damon.community.dto.AccessTokenDTO;
+import com.damon.community.dto.GithubUser;
+import com.damon.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("10b66927712f456c33dc");
+        accessTokenDTO.setClient_secret("a15577d66dedf397d1674de3af4ae827d6e8ba0d");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
