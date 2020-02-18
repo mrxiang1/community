074b73720051cfe4f19bd6ebbbfdfbf7b86f3fd4
DamonXiang <545052197@qq.com>
2020-02-18 17:42:34 +0800

    add h2 database

--- src/main/java/com/damon/community/controller/AuthorizeController.java
+++ src/main/java/com/damon/community/controller/AuthorizeController.java
@@ -2,11 +2,17 @@ package com.damon.community.controller;
 
 import com.damon.community.dto.AccessTokenDTO;
 import com.damon.community.dto.GithubUser;
+import com.damon.community.mapper.UserMapper;
+import com.damon.community.model.User;
 import com.damon.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -14,18 +20,44 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("10b66927712f456c33dc");
-        accessTokenDTO.setClient_secret("a15577d66dedf397d1674de3af4ae827d6e8ba0d");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null) {
+            User user = new User();
+            user.setName(githubUser.getName());
+            user.setToken(UUID.randomUUID().toString());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //登陆成功，写cookies和session
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        } else {
+            //登陆失败，重新登录
+            return "redirect:/";
+        }
     }
 }
